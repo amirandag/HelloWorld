2fa908a2117f902c455278bf91c2f0d0dfff6bf6
iaw46490689 <iaw46490689@h05.informatica.escoladeltreball.org>
2017-09-18 17:51:20 +0200

    First commit

---
+++ src/org/escoladeltreball/helloworld/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package org.escoladeltreball.helloworld;
+
+/**
+ * @author iaw46490689
+ *
+ */
+public final class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
