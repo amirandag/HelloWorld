3dc956afb6a8300467f90689a4a56724517d7d8b
iaw46490689 <iaw46490689@h05.informatica.escoladeltreball.org>
2017-09-20 16:33:56 +0200

    Interficie + implementacio

--- src/org/escoladeltreball/helloworld/Main.java
+++ src/org/escoladeltreball/helloworld/Main.java
@@ -7,13 +7,42 @@ package org.escoladeltreball.helloworld;
  * @author iaw46490689
  *
  */
-public final class Main {
+public final class Main implements Utils {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		int[] values = {0,1,3,5,-2,5};
+		Main m = new Main();
+		System.out.println(m.findSmallest(values));
+		System.out.print(m.sum(values));
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * This method returns the smallest value from an integer array.
+	 * @see org.escoladeltreball.helloworld.Utils#findSmallest(int[])
+	 */
+	@Override
+	public int findSmallest(int[] values) {
+		int smallest = values[0];
+		for (int i = 1; i < values.length; i++) {
+			if (smallest > values[i]) {
+				smallest = values[i];
+			}
+		}
+		return smallest;
 	}
 
+	@Override
+	public int sum(int[] values) {
+		int sum = 0;
+		for (int value:values) {
+			sum = sum + value;
+		}
+		return sum;
+	}
+	
 }
---
+++ src/org/escoladeltreball/helloworld/Utils.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package org.escoladeltreball.helloworld;
+
+/**
+ * @author iaw46490689
+ *
+ */
+public interface Utils {
+	
+	/*
+	 * This method returns the smallest value in an integer array.
+	 */
+	public abstract int findSmallest(int[] values);
+	
+	/*
+	 * This method sums the values in an integer array.
+	 */
+	public abstract int sum(int[] values);
+	
+}
